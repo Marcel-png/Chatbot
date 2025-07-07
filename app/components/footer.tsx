cc5e7a265317c39fd4d7d58eea489a6fc1fb1b76
Votre Nom <votre.email@exemple.com>
2025-07-07 21:13:25 +0200

    update

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,32 +18,35 @@ export default function Footer({
   setMessages,
 }: FooterProps) {
   return (
-    <footer className="bg-white shadow-lg w-11/12 md:w-3/4 rounded-xl h-20 flex items-center justify-between px-4 fixed bottom-4 left-1/2 transform -translate-x-1/2">
-      <button
-        onClick={() => {
-          localStorage.removeItem("chatMessages");
-          setMessages([]);
-        }}
-        className="ml-4 p-3 bg-red-500 mr-2 text-white rounded-full shadow-md hover:bg-red-600 transition-all flex items-center justify-center"
-        aria-label="Nettoyer"
-      >
-        <FaTrash size={24} />
-      </button>
+    <footer className="bg-white shadow-lg w-full md:w-3/4 max-w-2xl mx-auto rounded-xl h-auto min-h-20 flex flex-col md:flex-row items-center justify-between px-2 md:px-4 py-2 fixed bottom-0 left-1/2 transform -translate-x-1/2 gap-2 md:gap-0 z-20">
       <input
-        type="text"
+        type="search"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         onKeyDown={handleKeyDown}
-        className="flex-grow text-black border border-gray-300 rounded-full px-4 py-3 text-lg focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all"
+        className="flex-grow w-full md:w-auto text-black border border-gray-300 rounded-full px-4 py-3 text-lg focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all mb-2 md:mb-0"
         placeholder="Votre question ici..."
       />
-      <button
-        onClick={handleSend}
-        className="ml-4 p-3 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition-all flex items-center justify-center"
-        aria-label="Soumettre"
-      >
-        <FaArrowUp color="black" size={24} />
-      </button>
+      <div className="flex flex-row gap-2 w-full md:w-auto justify-end ml-2">
+        <button
+          onClick={() => {
+            alert("Votre discussion va être supprimer!")
+            localStorage.removeItem("chatMessages");
+            setMessages([]);
+          }}
+          className="p-3 bg-red-500 text-white rounded-full shadow-md hover:bg-red-600 transition-all flex items-center justify-center"
+          aria-label="Nettoyer"
+        >
+          <FaTrash size={24} />
+        </button>
+        <button
+          onClick={handleSend}
+          className="p-3 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition-all flex items-center justify-center"
+          aria-label="Soumettre"
+        >
+          <FaArrowUp size={24} />
+        </button>
+      </div>
     </footer>
   );
 }
